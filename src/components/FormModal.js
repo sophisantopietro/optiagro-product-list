4f1cf3b4aa83fcf862bac65c0a487ef61b1365cc
Sophia Santopietro <sophia@yellowspot.com.ar>
2019-06-03 10:33:47 -0300

    change style class name and delete inecesarie field

--- src/components/FormModal.js
+++ src/components/FormModal.js
@@ -16,8 +16,6 @@ class FormModal extends Component {
     };
   }
 
-  
-
   handleSubmit = (e) => {
     e.preventDefault()
     if(this.props.isUnique(this.state.name)){
@@ -33,7 +31,6 @@ class FormModal extends Component {
         error: true
       })
     }
-      
   }
 
   handleChange = (e, { name, value }) => this.setState({ [name]: value })
@@ -42,15 +39,15 @@ class FormModal extends Component {
     const {name, amount, description, error} = this.state;
     return (
       <Modal
-        trigger={<Button color='teal' className='addButton' onClick={this.props.handleOpen}>Add Item</Button>}
+        trigger={<Button color='teal' className='add-button' onClick={this.props.handleOpen}>Add Item</Button>}
         open={this.props.modalOpen}
         onClose={this.props.handleClose}
       >
         <Header icon='browser' content='Cookies policy' />
         <Modal.Content>
           <Form onSubmit={this.handleSubmit} error={error}>
-            <Form.Input label='Name' placeholder='Name' name='name' value={name} onChange={this.handleChange} />     
-            <Form.Input label='Amount' placeholder='Amount' name='amount' value={amount} onChange={this.handleChange} />     
+            <Form.Input label='Name' placeholder='Name' name='name' value={name} onChange={this.handleChange} />
+            <Form.Input label='Amount' placeholder='Amount' name='amount' value={amount} onChange={this.handleChange} />
             <Form.TextArea label='Description' placeholder='Description' name='description' value={description} onChange={this.handleChange}/>
             <Message
               error
@@ -58,8 +55,8 @@ class FormModal extends Component {
               content='Name is already taken.'
             />
             <Button color='teal' type='submit' disabled={!name || !amount || !description}>Submit</Button>
-          </Form>    
-        </Modal.Content> 
+          </Form>
+        </Modal.Content>
       </Modal>
     )
   }
